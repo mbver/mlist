541487cfdbe00919a422eb163f84ab5c3e7fafc9
phuoc <phuocdongxoai@gmail.com>
2024-10-11 18:08:11 +0700

    setup subnet

---
+++ scripts/setup_subnet.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Check if loopback is setup
+action=${1:-up}
+
+if [  "$action" = "up" ]
+then
+  # Check if loopback is setup
+  ping -c 1 -W 10 127.0.0.2 > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+      exit
+  fi
+fi
+
+
+# If we're not on OS X, then error
+case $OSTYPE in
+    darwin*)
+        ;;
+    *)
+        echo "Can't setup interfaces on non-Mac. Error!"
+        exit 1
+        ;;
+esac
+
+# first loopback, use intensively
+for ((i=10;i<256;i++))
+  do
+      if [ "$action" = "up" ]
+      then
+        sudo ifconfig lo0 alias 127.0.0.$i up
+      else
+        sudo ifconfig lo0 127.0.0.$i delete
+     fi
+  done
+
+# Not use much, only need a few
+for j in 1 2
+do
+  for ((i=10;i<15;i++))
+  do
+      if [ "$action" = "up" ]
+      then
+        sudo ifconfig lo0 alias 127.0.$j.$i up
+      else
+        sudo ifconfig lo0 127.0.$j.$i delete
+     fi
+  done
+done
