be9391e71d9dc5aff5dffd54709413ac561f688e
phuoc <phuocdongxoai@gmail.com>
2024-10-27 16:45:13 +0700

    more deadnode test

--- state_test.go
+++ state_test.go
@@ -6,6 +6,94 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestMemberlist_DeadNode_AlreadyDead(t *testing.T) {
+	m, cleanup, err := newTestMemberlistNoSchedule()
+	defer cleanup()
+	require.Nil(t, err)
+
+	a := alive{
+		Lives: 1,
+		ID:    "test",
+		IP:    []byte{127, 0, 0, 1},
+		Port:  7946,
+	}
+	m.aliveNode(&a, nil)
+
+	d := dead{
+		Lives: 1,
+		ID:    "test",
+	}
+	m.deadNode(&d, nil)
+
+	d.Lives = 2
+	m.deadNode(&d, nil)
+
+	node := m.GetNodeState("test")
+	require.Equal(t, StateDead, node.State)
+
+	msgs := dumpQueue(m.mbroadcasts)
+	selected := []*TransmitCapItem{}
+	for _, m := range msgs {
+		if m.name == "test" {
+			selected = append(selected, m)
+		}
+	}
+	require.Equal(t, 1, len(selected))
+	msg := selected[0]
+	require.Equal(t, deadMsg, msgType(msg.msg[0]))
+	err = decode(msg.msg[1:], &d)
+	require.Nil(t, err)
+	require.Equal(t, 1, int(d.Lives))
+}
+
+func TestMemberlist_DeadNode_OldDead(t *testing.T) {
+	m, cleanup, err := newTestMemberlistNoSchedule()
+	defer cleanup()
+	require.Nil(t, err)
+
+	a := alive{
+		Lives: 10,
+		ID:    "test",
+		IP:    []byte{127, 0, 0, 1},
+		Port:  7946,
+	}
+	m.aliveNode(&a, nil)
+
+	d := dead{
+		Lives: 1,
+		ID:    "test",
+	}
+	m.deadNode(&d, nil)
+
+	node := m.GetNodeState("test")
+	require.Equal(t, StateAlive, node.State)
+}
+
+func TestMemberlist_DeadNode_OldAlive(t *testing.T) {
+	m, cleanup, err := newTestMemberlistNoSchedule()
+	defer cleanup()
+	require.Nil(t, err)
+
+	a := alive{
+		Lives: 10,
+		ID:    "test",
+		IP:    []byte{127, 0, 0, 1},
+		Port:  7946,
+	}
+	m.aliveNode(&a, nil)
+
+	d := dead{
+		Lives: 10,
+		ID:    "test",
+	}
+	m.deadNode(&d, nil)
+
+	m.aliveNode(&a, nil)
+
+	node := m.GetNodeState("test")
+	require.Equal(t, StateDead, node.State)
+}
+
 func TestMemberlist_DeadNodeRefute(t *testing.T) {
 	m, cleanup, err := newTestMemberlistNoSchedule()
 	defer cleanup()
