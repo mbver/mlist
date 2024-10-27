6c63e2ab984df535823f878da931e0b14ae68894
phuoc <phuocdongxoai@gmail.com>
2024-10-27 10:53:09 +0700

    test memberlist pushpull

--- maintenance_test.go
+++ maintenance_test.go
@@ -1,8 +1,13 @@
 package memberlist
 
 import (
+	"fmt"
+	"net"
 	"reflect"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestPickRandomNodes(t *testing.T) {
@@ -49,6 +54,50 @@ func TestPickRandomNodes(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestMemberlist_PushPull(t *testing.T) {
+	m1, cleanup1, err := newTestMemberlistNoSchedule()
+	defer cleanup1()
+	require.Nil(t, err)
+
+	m2, cleanup2, err := newTestMemberlistNoSchedule()
+	defer cleanup2()
+	require.Nil(t, err)
+
+	joinAndTest(t, m1, m2)
+
+	for i := 0; i < 3; i++ {
+		a := alive{
+			Lives: 1,
+			ID:    fmt.Sprintf("Test %d", i),
+			IP:    net.ParseIP(m1.config.BindAddr),
+			Port:  uint16(m1.config.BindPort),
+		}
+		m1.aliveNode(&a, nil)
+	}
+	success, msg := retry(func() (bool, string) {
+		m1.pushPull()
+		time.Sleep(10 * time.Millisecond)
+		if m2.NumActive() != 5 {
+			return false, "expect 5 nodes"
+		}
+		nodes := m2.ActiveNodes()
+		sortNodes(nodes)
+		for i := 0; i < 3; i++ {
+			if nodes[i+2].ID != fmt.Sprintf("Test %d", i) {
+				return false, "wrong node id"
+			}
+			if nodes[i+2].IP.String() != m1.config.BindAddr {
+				return false, "wrong node ip"
+			}
+			if nodes[i+2].Port != uint16(m1.config.BindPort) {
+				return false, "wrong node port"
+			}
+		}
+		return true, ""
+	})
+	require.True(t, success, msg)
 }
 
 func TestProbeNode(t *testing.T) {}
--- pushpull_test.go
+++ pushpull_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPushPull(t *testing.T) {
+func TestPushPull_SendReceive(t *testing.T) {
 	m1, cleanup1, err := newTestMemberlistNoSchedule()
 	defer cleanup1()
 	require.Nil(t, err)
